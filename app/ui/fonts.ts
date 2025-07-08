cf28a054f30692f4fdd3ca467ff54004dba5f72c
KukkikD <adeezon9@gmail.com>
2025-07-08 01:14:47 -0700

    create next.js dashboard / chapter 1-5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google'; /* Primary Font */
+import { Lusitana } from 'next/font/google'; /* Secondary Font */
+
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
